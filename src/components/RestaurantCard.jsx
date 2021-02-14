133bbb8f1dbaef1d6725c548f4b4528f94dd98ad
alisaidgunes <alisaidgunes1@gmail.com>
2021-02-14 19:50:05 +0300

    yemek kart dizaynı

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -4,7 +4,35 @@ export default function RestaurantCard(props) {
   return (
     <div>
       <Container fluid>
-        <Row className=" text-center">
+        {props.cityRest.map((item, index) => (
+          <Row className="border w-50 mx-auto my-3">
+            <Col sm={4}>
+              {item.restaurant.featured_image ? (
+                <Image
+                  className="pt-3"
+                  style={{ width: "200px", height: "200px" }}
+                  src={item.restaurant.featured_image}
+                  thumbnail
+                />
+              ) : (
+                <Image
+                  style={{ width: "200px", height: "200px" }}
+                  src="https://b.zmtcdn.com/images/res_avatar_476_320_1x_new.png?output-format=webp%22"
+                  className="my-3"
+                  thumbnail
+                />
+              )}
+            </Col>
+            <Col sm={8}>
+              <h2 className="py-5 fs-4 text-dark">{item.restaurant.name}</h2>
+            </Col>
+          </Row>
+        ))}
+      </Container>
+    </div>
+  );
+}
+/*  <Row className=" text-center">
           {props.cityRest.map((item, index) => (
             <Col key={index} xs={12} sm={12} md={6} lg={4} xl={3}>
               {console.log(item.restaurant.name)}
@@ -27,11 +55,8 @@ export default function RestaurantCard(props) {
               </div>
             </Col>
           ))}
-        </Row>
-      </Container>
-    </div>
-  );
-}
+        </Row> */
+
 /* 
             return (
               <Col key={index} xs={12} sm={12} md={6} lg={4} xl={3}>
