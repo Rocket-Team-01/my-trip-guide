a97552d7273b645e07a328c11d17a904cfe92b86
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-02-14 14:22:17 +0300

    pls

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import {useParams}  from 'react-router-dom'
+import List from "../components/List";
 
 function Search(props) {
 
-    const [data, setData] = useState([]);
 
     const params = useParams();
     const { query } = params;
+    /*
     console.log(`&query= ${query}`)
-
+    */
     const [city, setCity] = useState([]);
 
 
@@ -25,16 +26,51 @@ function Search(props) {
         fetch(`https://developers.zomato.com/api/v2.1/cities?q=${query}\n`, requestOptions)
             .then(response => response.json())
             .then(result =>{
+                /*
                 console.log(result.location_suggestions[0].id);
+                */
                 setCity(result.location_suggestions[0].id);
-
+                /*
+                console.log(city);
+                */
             })
             .catch(error => console.log('error', error));
-            console.log("calıstı")
+            
     }, [])
 
     
-     useEffect(() => {
+     
+     
+
+
+    return (
+        <div>
+            <List city = {city} />
+        </div>
+    )
+}
+
+
+
+export default Search;
+
+
+
+
+/* 
+<h1>Efekan</h1>
+            <h1>{query}</h1>
+            <h5>{data.map((item,index)=> (
+                <div key={index} >
+                    <p>{item.restaurant.name}</p>
+                </div>
+            ))}</h5>
+*/
+
+
+
+/* 
+useEffect(() => {
          var requestOptions = {
              method: 'GET',
              headers: {
@@ -47,23 +83,11 @@ function Search(props) {
          fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${city}&entity_type=city&sort=rating&order=dsc`, requestOptions)
              .then(response => response.json())
              .then(result =>{
-                 setData(result);
+                 setData(result.restaurants);
                  console.log(result.restaurants);
              })
              .catch(error => console.log('error', error));
-             console.log("calıstı")
+            
      }, [])
-     
 
-
-    return (
-        <div>
-            <h1>Efekan</h1>
-            <h1>{query}</h1>
-        </div>
-    )
-}
-
-
-
-export default Search;
+*/
