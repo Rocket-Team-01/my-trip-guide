3605862ec79ed5f4020509c0a0ee5f4d98dc4f2b
Emine Kebeli <76777299+eminekbl@users.noreply.github.com>
2021-02-14 14:46:41 +0300

    getCityRest

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,10 +10,11 @@ function Search(props) {
     /*
     console.log(`&query= ${query}`)
     */
-    const [city, setCity] = useState([]);
+    // const [city, setCity] = useState([]);
+    const [cityRest, setCityRest] = useState([]);
 
-
-    useEffect(() => {
+    function getCityID(){
+        
         var requestOptions = {
             method: 'GET',
             headers: {
@@ -29,13 +30,45 @@ function Search(props) {
                 /*
                 console.log(result.location_suggestions[0].id);
                 */
-                setCity(result.location_suggestions[0].id);
+               getCityRest(result.location_suggestions[0].id)
+                // setCity(result.location_suggestions[0].id);
                 /*
                 console.log(city);
                 */
             })
             .catch(error => console.log('error', error));
-            
+    }
+
+    function getCityRest(event){
+        var requestOptions = {
+            method: 'GET',
+            headers: {
+                "user-key": "415c9b42482714e36aa1e285b7ab62be",
+                "content-type": "application/json",
+              },
+            redirect: 'follow'
+        };
+
+        fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=${event}&entity_type=city&sort=rating&order=dsc`, requestOptions)
+            .then(response => response.json())
+            .then(result =>{
+                /*
+                console.log(result.location_suggestions[0].id);
+                */
+                setCityRest(result.restaurants);
+                // console.log(result)
+                // console.log(`cityRest:: ${cityRest}`);
+                
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    
+    useEffect(() => {
+       
+        getCityID()
+        
+
     }, [])
 
     
@@ -45,7 +78,12 @@ function Search(props) {
 
     return (
         <div>
-            <List city = {city} />
+            {/* <List city = {city} /> */}
+            {cityRest.map((item,index)=> (
+                <div key={index} >
+                    <p>{item.restaurant.name}</p>
+                </div>
+            ))}
         </div>
     )
 }
