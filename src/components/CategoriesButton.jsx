ce11bb64c42432e7fb377370cea5b0703fb80c2c
Emine Kebeli <76777299+eminekbl@users.noreply.github.com>
2021-02-15 16:16:51 +0300

    CategoriesButton.css

--- src/components/CategoriesButton.jsx
+++ src/components/CategoriesButton.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Dropdown, FormControl, Button } from "react-bootstrap";
 import categories from "../data/categories";
-
+import '../css/CategoriesButton.css'
 function CategoriesButton(props) {
   const [title, setTitle] = useState("Categories");
 
@@ -34,12 +34,13 @@ function CategoriesButton(props) {
             autoFocus
             className="mx-3 my-2 w-auto"
             placeholder="Type to filter..."
-            onChange={(e) => setValue(e.target.value)}
+            onChange={(e) => setValue(e.target.value.toLowerCase())}
             value={value}
           />
-          <ul className="list-unstyled">
+          <ul>
             {React.Children.toArray(children).filter(
               (child) =>
+         
                 !value || child.props.children.toLowerCase().startsWith(value)
             )}
           </ul>
@@ -49,13 +50,14 @@ function CategoriesButton(props) {
   );
 
   return (
-    <Dropdown>
+    <Dropdown >
       <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
         {title}
       </Dropdown.Toggle>
-      <Dropdown.Menu as={CustomMenu}>
+      <Dropdown.Menu className='dropdown-category' as={CustomMenu}>
         {categories.map((item, index) => (
           <Dropdown.Item
+            
             key={index}
             eventKey={item.cuisine_id}
             onSelect={() => {
