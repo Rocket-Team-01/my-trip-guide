f35464ea166a323292ebc66ebc74a5643753ae86
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-02-15 22:16:33 +0300

    cardchanges

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,89 +1,49 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Container, Image, Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Container, Image } from "react-bootstrap";
 import "../css/Home.css";
-import $ from "jquery";
-
 export default function RestaurantCard(props) {
-  
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [cost, setCost] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [timing, setTiming] = useState("");
+  const [address, setAddress] = useState("");
 
   return (
-    <div>
+    <div >
       <Container>
         <Row>
           {props.cityRest.map((item, index) => (
-            <Row key={index} className="w-50 my-3  mx-auto">
-              <Col xs={12} sm={12} md={12} lg={6} xl={4}>
+            <Row key={index} className="w-50 my-3 mx-auto justify-content-center">
+              <Col xs={12} sm={12} md={12} lg={6} xl={5} >
                 {item.restaurant.featured_image ? (
                   <Image
-                    className="my-3 mx-1 rounded-3 border border-dark"
-                    style={{ width: "170px", height: "120px" }}
+                    className="my-3  rounded-3 border border-dark"
+                    style={{ width: "200px", height: "150px" }}
                     src={item.restaurant.featured_image}
                   />
                 ) : (
                   <Image
-                    style={{ width: "170px", height: "120px" }}
+                    style={{ width: "200px", height: "150px" }}
                     src="https://b.zmtcdn.com/images/res_avatar_476_320_1x_new.png?output-format=webp%22"
-                    className="my-3 mx-2 rounded-3 border border-dark"
+                    className="my-3  rounded-3 border border-dark"
                   />
                 )}
               </Col>
-              <Col xs={12} sm={12} md={0} lg={6} xl={8} className="pt-3">
+              <Col xs={12} sm={12} md={0} lg={6} xl={5} className="pt-3">
                 <h3 className="text-dark fs-5 font-weight-bold">
                   {item.restaurant.name}
                 </h3>
                 <h6 className="text-dark">
-                  Phone Number: {item.restaurant.phone_numbers.slice(0, 12)}
+                  Phone: {item.restaurant.phone_numbers.slice(0, 12)}
                 </h6>
-                <h6 className="text-dark">
-                  Rating: {item.restaurant.user_rating.aggregate_rating} (
-                  {item.restaurant.user_rating.votes})
+                <h6 className="text-dark py-1">
+                  <span
+                  className="p-2 rounded-3 text-white" 
+                  style={{"backgroundColor": `#${item.restaurant.user_rating.rating_color}`}} >
+                  {item.restaurant.user_rating.aggregate_rating} 
+                  </span> 
                 </h6>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-toggle="modal"
-                  data-target="#exampleModal"
-                >
-                  Details
-                </button>
-                <div
-                  className="modal fade"
-                  id="exampleModal"
-                  tabIndex="-1"
-                  aria-labelledby="exampleModalLabel"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                          {item.restaurant.name}
-                        </h5>
-                        <button
-                          type="button"
-                          className="close"
-                          data-dismiss="modal"
-                          aria-label="Close"
-                        >
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div className="modal-body">...</div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="button" className="btn btn-primary">
-                          Save changes
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -104,7 +64,74 @@ export default function RestaurantCard(props) {
                 >
                   <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z" />
                 </svg>
-                
+                <button
+                  type="button"
+                  className="btn btn-primary ml-2"
+                  data-toggle="modal"
+                  data-target=".bd-example-modal-lg"
+                  onClick={() => {
+                    setName(item.restaurant.name);
+                    setNumber(item.restaurant.phone_numbers.slice(0, 12));
+                    setCost(item.restaurant.average_cost_for_two);
+                    setCurrency(item.restaurant.currency);
+                    setTiming(item.restaurant.timings);
+                    setAddress(item.restaurant.location.address);
+                  }}
+                >
+                  Details
+                </button>
+                <div
+                  className="modal fade bd-example-modal-lg"
+                  id="exampleModal"
+                  tabIndex="-1"
+                  aria-labelledby="myLargeModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog modal-dialog-centered modal-lg">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                          {name}{" "}
+                          <span className="ml-3 bg-success rounded-3 text-white p-1">
+                            {number} <i class="fas fa-phone"></i>
+                          </span>
+                        </h5>
+                        <button
+                          type="button"
+                          className="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        Average cost For Two People:{" "}
+                        <span className="data">
+                          {cost} {currency}
+                        </span>{" "}
+                        <br />
+                        Opening Hours: <span className="data">
+                          {timing}
+                        </span>{" "}
+                        <br />
+                        Address: <span className="data">{address}</span>
+                      </div>
+                      <div className="modal-footer mx-auto">
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          data-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" className="btn btn-success">
+                          Add to Favorites
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </Col>
             </Row>
           ))}
@@ -114,3 +141,7 @@ export default function RestaurantCard(props) {
   );
 }
 
+
+/*
+{item.restaurant.user_rating.votes}
+*/
